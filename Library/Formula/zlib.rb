0024979155e75763a24450f1dab2899f73102f90
Jamie van Dyke <fearoffish@fearofpro.local>
2009-09-22 10:23:14 +0100

    added zlib

---
+++ Library/Formula/zlib.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Zlib <Formula
+  @url='http://www.zlib.net/zlib-1.2.3.tar.gz'
+  @homepage='http://www.zlib.net/'
+  @md5='debc62758716a169df9f62e6ab2bc634'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
