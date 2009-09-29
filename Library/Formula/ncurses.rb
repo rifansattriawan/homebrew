ac45022e5dccbc04527f5ee361e7edc0bd0237bb
Adam Vandenberg <flangy@gmail.com>
2009-09-28 19:44:40 -0700

    Formula for ncurses.

---
+++ Library/Formula/ncurses.rb
@@ -0,0 +1,19 @@
+require 'brewkit'
+
+class Ncurses <Formula
+  url 'http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.7.tar.gz'
+  homepage 'http://www.gnu.org/software/ncurses/'
+  md5 'cce05daf61a64501ef6cd8da1f727ec6'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", 
+                          "--disable-debug", 
+                          "--disable-dependency-tracking",
+                          "--disable-echo",
+                          "--without-ada",
+                          "--enable-widec",
+                          # tic doesn't link correctly
+                          "--without-progs"
+    system "make install"
+  end
+end
