a52d9b7abf4144377ac477287404e2d45da17cee
Adam Vandenberg <flangy@gmail.com>
2009-09-29 15:25:49 -0700

    Formula for bison (2.4.1; OS X 10.5 comes with 2.3)
    Bison is a general-purpose parser generator that converts an annotated
    context-free grammar into an LALR(1) or GLR parser for that grammar. Once
    you are proficient with Bison, you can use it to develop a wide range of
    language parsers, from those used in simple desk calculators to complex
    programming languages.

---
+++ Library/Formula/bison.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Bison <Formula
+  url 'http://ftp.gnu.org/gnu/bison/bison-2.4.1.tar.bz2'
+  homepage 'http://www.gnu.org/software/bison/'
+  md5 '84e80a2a192c1a4c02d43fbf2bcc4ca4'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
