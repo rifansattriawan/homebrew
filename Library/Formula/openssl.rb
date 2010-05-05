e53051bbe29c5a87678fcd8b0fbd381141cf53d7
Adam Vandenberg <flangy@gmail.com>
2010-05-05 14:43:30 -0700

    openssl bumped

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Openssl <Formula
-  url 'http://www.openssl.org/source/openssl-0.9.8k.tar.gz'
-  version '0.9.8k'
+  url 'http://www.openssl.org/source/openssl-0.9.8n.tar.gz'
+  version '0.9.8n'
   homepage 'http://www.openssl.org'
-  md5 'e555c6d58d276aec7fdc53363e338ab3'
+  md5 '076d8efc3ed93646bd01f04e23c07066'
   
   def keg_only?
     :provided_by_osx
@@ -12,9 +12,8 @@ class Openssl <Formula
 
   def install
     ENV.j1
-    
-    system "./config",  "--prefix=#{prefix}", "--openssldir=#{etc}",
-                          "zlib-dynamic", "shared"
+    system "./config", "--prefix=#{prefix}", "--openssldir=#{etc}",
+                       "zlib-dynamic", "shared"
     system "make"
     system "make test"
     system "make install"
