34f525a27c8d060f09cd3317dde2d62016ec14b0
Adam Vandenberg <flangy@gmail.com>
2010-05-26 08:49:53 -0700

    wip - Wine 64bit

--- Library/Formula/wine.rb
+++ Library/Formula/wine.rb
@@ -1,8 +1,14 @@
 require 'formula'
+require 'hardware'
 
 class Wine <Formula
-  url 'http://downloads.sourceforge.net/project/wine/Source/wine-1.1.42.tar.bz2'
-  sha1 'ea932f19528a22eacc49f16100dbf2251cb4ad5c'
+  if MACOS_VERSION >= 10.6 and Hardware.is_64_bit?
+    url 'http://prdownloads.sourceforge.net/wine/wine-1.2-rc1.tar.bz2'
+    sha1 '31ea3a75ea560cd486fb58825242ac0a6fc664ca'
+  else
+    url 'http://downloads.sourceforge.net/project/wine/Source/wine-1.1.42.tar.bz2'
+    sha1 'ea932f19528a22eacc49f16100dbf2251cb4ad5c'
+  end
   homepage 'http://www.winehq.org/'
   head 'git://source.winehq.org/git/wine.git'
 
@@ -21,18 +27,23 @@ EOS
     ENV.gcc_4_2
     ENV.x11
 
-    # Make sure we build 32bit version, because Wine64 is not fully functional yet
-    build32 = "-arch i386 -m32"
-
     ENV["LIBS"] = "-lGL -lGLU"
-    ENV.append "CFLAGS", build32
-    ENV.append "CXXFLAGS", "-D_DARWIN_NO_64_BIT_INODE"
-    ENV.append "LDFLAGS", [build32, "-framework CoreServices", "-lz", "-lGL -lGLU"].join(' ')
+    ENV.append "LDFLAGS", ["-framework CoreServices", "-lz", "-lGL -lGLU"].join(' ')
+
+    if MACOS_VERSION >= 10.6 and Hardware.is_64_bit?
+      # ENV.m64
+    else
+      build32 = "-arch i386 -m32"
+      ENV.append "CFLAGS", build32
+      ENV.append "CXXFLAGS", "-D_DARWIN_NO_64_BIT_INODE"
+      ENV.append "LDFLAGS", build32
+    end
+
     ENV.append "DYLD_FALLBACK_LIBRARY_PATH", "/usr/X11/lib"
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--disable-win16"
+    args = ["--prefix=#{prefix}", "--disable-win16"]
+    args << "--without-freetype" << "--enable-win64" if MACOS_VERSION >= 10.6 and Hardware.is_64_bit?
+    system "./configure", *args
     system "make install"
 
     # Use a wrapper script, so rename wine to wine.bin
