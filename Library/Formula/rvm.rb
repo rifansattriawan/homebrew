6c23229ba065a001051a34d6dfc210a0c5add097
Adam Vandenberg <flangy@gmail.com>
2010-06-23 13:56:55 -0700

    RVM - additional tweaks

--- Library/Formula/rvm.rb
+++ Library/Formula/rvm.rb
@@ -1,12 +1,41 @@
 require 'formula'
 
 class Rvm <Formula
-  url 'http://rvm.beginrescueend.com/releases/rvm-0.1.38.tar.gz'
+  url 'http://rvm.beginrescueend.com/releases/rvm-0.1.40.tar.gz'
   homepage 'http://rvm.beginrescueend.com/'
-  md5 '9ee11f8d006321ae33bc06571334e611'
+  md5 '2cff08697543c28267a11e89a1082a9a'
+
+  # Don't kill empty folders
+  def skip_clean? path
+    true
+  end
 
   def install
-    ln_s prefix, "#{ENV['HOME']}/.rvm"
-    system "./install", "--auto", "--prefix #{prefix}/"
+    # TODO
+    # If ~/.rvm exists, and is a symlink to the cellar (or broken symlink)
+    # then remove it and continue.
+    # If it is a real folder with existing rvm stuff in it, then exit instead
+    # of munging a user's existing install.
+
+    (prefix+'rvm').mkpath
+    ln_s (prefix+'rvm'), "#{ENV['HOME']}/.rvm"
+    system "./install", "--prefix", "#{prefix}/"
+
+    bin.mkpath
+    Dir.chdir prefix do
+      Dir['rvm/bin/*'].each do |p|
+        ln_s prefix+p, bin
+      end
+    end
+  end
+
+  def caveats
+    <<-EOS.undent
+      This formula created a symlink ~/.rvm that points to:
+        #{prefix}
+
+      To enable rvm you'll want to add this to your .bashrc or other shell profile:
+        [[ -s "#{prefix}/scripts/rvm" ]] && source "#{prefix}/scripts/rvm"
+    EOS
   end
 end
