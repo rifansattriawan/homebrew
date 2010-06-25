be11766b1e7f8f87fb2e5f441afd6c8a49035064
Adam Vandenberg <flangy@gmail.com>
2010-06-25 09:46:02 -0700

    Update bison to 2.4.2

--- Library/Formula/bison.rb
+++ Library/Formula/bison.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Bison <Formula
-  url 'http://ftp.gnu.org/gnu/bison/bison-2.4.1.tar.bz2'
+  url 'http://ftp.gnu.org/gnu/bison/bison-2.4.2.tar.bz2'
   homepage 'http://www.gnu.org/software/bison/'
-  md5 '84e80a2a192c1a4c02d43fbf2bcc4ca4'
+  md5 '63584004613aaef2d3dca19088eb1654'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
