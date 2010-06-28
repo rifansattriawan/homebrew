939a34797e0b582f061bbbf88eef0592ed3f1f4b
Chris Adams <chris@improbable.org>
2010-06-28 13:59:49 -0400

    Update curl to 7.21
    This was necessary because the version which ships with Mac OS X 10.6 is
    old enough to have a bug which causes PyCurl to always report the
    response headers as empty.

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Curl <Formula
-  url 'http://curl.haxx.se/download/curl-7.19.7.tar.bz2'
+  url 'http://curl.haxx.se/download/curl-7.21.0.tar.bz2'
   homepage 'http://curl.haxx.se/'
-  md5 '79a8fbb2eed5464b97bdf94bee109380'
+  md5 'e1a2a773e93a39f3c04cab92c55bf197'
   aka 'libcurl'
 
   depends_on 'libssh2' => :optional
